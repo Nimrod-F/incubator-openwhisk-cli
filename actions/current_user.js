fb2983e4b8f3cf739094a1675356c8d0849ad392
Nimrod Foldvari <foldvari_nimrod@yahoo.com>
2025-05-15 22:45:02 +0300

    if expr example

---
+++ actions/current_user.js
@@ -0,0 +1,10 @@
+function main(params) {
+
+    return {
+        output: {
+            q_number:   params.q_number,
+            name:       "Nimrod Foldvari",
+            age: 18
+        }
+    };
+}
